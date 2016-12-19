b466509162751baacfe01cbe5cc5c59963376f7b
Rails Course Test Site <glitcher808@gmail.com>
2016-12-19 17:00:09 +0000

    edited db a bit and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208114246) do
+ActiveRecord::Schema.define(version: 20161219152210) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "email"
   end
 
 end
